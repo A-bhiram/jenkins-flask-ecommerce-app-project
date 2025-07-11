b112c31f63836eab8451b8d448108c8117c30ce5
A-bhiram <abhirammanojofficial@gmail.com>
2025-07-11 15:30:20 +0000

    Update greeting message

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, this is your first Flask e-commerce app!'
+    return 'Hello, this is my first Flask e-commerce app!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
