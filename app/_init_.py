16280876a7d82c12426645eec43fdc10830ea492
A-bhiram <abhirammanojofficial@gmail.com>
2025-07-18 19:17:55 +0000

    Reset: fresh start for Flask e-commerce project

--- app/_init_.py
+++ app/_init_.py
@@ -1,15 +1,7 @@
-# app/__init__.py
-
 from flask import Flask
-from .models import db
-from .routes import main as main_blueprint
+from app.routes import main
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(app)
-    app.register_blueprint(main_blueprint)
-
+    app = Flask(_name_)
+    app.register_blueprint(main)
     return app
