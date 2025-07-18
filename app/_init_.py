156186e28be9f9cd6b244498b5794b9209243d8c
A-bhiram <abhirammanojofficial@gmail.com>
2025-07-18 18:32:05 +0000

    Add SQLAlchemy setup and product model

--- app/_init_.py
+++ app/_init_.py
@@ -1,7 +1,15 @@
+# app/__init__.py
+
 from flask import Flask
-from app.routes import main
+from .models import db
+from .routes import main as main_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(main)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    app.register_blueprint(main_blueprint)
+
     return app
